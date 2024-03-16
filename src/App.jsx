30f5f49b0333388c16f21cee0be0af482abff028
rohit-sharma0603 <sharmarohit061997@gmail.com>
2024-03-16 22:37:51 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,71 @@
-function App(){
+import React, {useState, useEffect} from 'react'
+import PokemonContainer from './components/PokemonContainer';
 
-    
+import PokemonModal from './components/PokemonModal';
 
+export default function App() {
+    const [pokemons, setPokemons] = useState([]);
+    const [currentPokemonAPI, setCurrentPokemonAPI] = useState("https://content.newtonschool.co/v1/pr/64ccef982071a9ad01d36ff6/pokemonspages1");
+    const [selectPokemonInModal, setSelectedPokemonInModal] = useState(null);
+    const [isLoading, setIsLoading] = useState(true)
+
+    function assignPokemonToModal(assignedPokemon){
+        setSelectedPokemonInModal(assignedPokemon);
+    }
+    function closeModal(){
+        setSelectedPokemonInModal("")
+    }
+    async function getPokemonData(){
+        //handle negative cases first -> this makes your code very clean
+        if(!currentPokemonAPI){
+            return;
+        }
+        try{
+            const response = await fetch(currentPokemonAPI);
+            if(!response.ok){
+                throw new Error("Something went wrong")
+            }
+            const pokemonData = await response.json();
+            // console.log(pokemonData);
+            // console.log(pokemonData[0].results);
+            setCurrentPokemonAPI(pokemonData[0]?.next ? pokemonData[0]?.next : null)
+            const apiArray = pokemonData[0].results.map((pokemon)=>{
+                return fetch(pokemon.url)
+            })
+            // console.log(apiArray);
+            const pokemonStatsResponse = await Promise.all(apiArray);
+            const pokemonStatsJsonPromises = pokemonStatsResponse.map((soloApiResponse)=>{
+                if(!soloApiResponse.ok){
+                    throw new Error("Something went wrong")
+                }
+                return soloApiResponse.json();
+            })
+            // console.log(pokemonStatsJsonPromises)
+            const pokemonStats = await Promise.all(pokemonStatsJsonPromises);
+            console.log(pokemonStats);
+            const formatedPokemonStats = pokemonStats.map((pokemonStat)=>{
+                return pokemonStat[0]
+            })
+            console.log(formatedPokemonStats);
+            setPokemons((prev)=> [...prev, ...formatedPokemonStats]);
+            setIsLoading(false);
+
+        }
+        catch(err){
+            console.error(err)
+        }
+    }
+    useEffect(()=>{
+        getPokemonData()
+    },[])
+  return (
+    <div className='container'>
+        {selectPokemonInModal ? <PokemonModal pokemonData={selectPokemonInModal}/> : null}
+
+        <h1>Pokemon Kingdom</h1>
+        {!isLoading ? <PokemonContainer pokemons={pokemons} assignPokemonToModal={assignPokemonToModal} closeModal={closeModal}/> : 'Loading....'}
+        {!isLoading && currentPokemonAPI ? <button onClick={getPokemonData}>More Pokemons</button> : null}
+
+    </div>
+  )
 }
-
-export default App;
-
-
---
+++ src/components/PokemonModal.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './modal.css'
+export default function PokemonModal({pokemonData, closeModal}) {
+  console.log(pokemonData)
+  return ReactDOM.createPortal(
+    <div className='modal'>
+      <div className='content'>
+        abcd
+        <button onClick={closeModal}>close modal</button>
+      </div>
+      
+    </div>,
+    document.getElementById('portal')
+  )
+}
